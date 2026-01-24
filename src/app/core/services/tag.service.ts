ef021e25ff743eab90b56fd93d1c5a6e92a5dad1
wpearce <pearce.oliver@gmail.com>
2026-01-24 22:23:15 +0100

    Cache tag list for two minutes

--- src/app/core/services/tag.service.ts
+++ src/app/core/services/tag.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of, shareReplay, tap } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { Tag } from '../models/message-template';
 
@@ -8,8 +8,30 @@ import { Tag } from '../models/message-template';
 export class TagService {
   private readonly http = inject(HttpClient);
   private readonly tagsBaseUrl = `${environment.apiBaseUrl}/tags`;
+  private readonly cacheDurationMs = 2 * 60 * 1000;
+  private cachedTags: Tag[] | null = null;
+  private lastFetchedAt = 0;
+  private inFlight$: Observable<Tag[]> | null = null;
 
   getAll(): Observable<Tag[]> {
-    return this.http.get<Tag[]>(this.tagsBaseUrl);
+    const now = Date.now();
+    if (this.cachedTags && now - this.lastFetchedAt < this.cacheDurationMs) {
+      return of(this.cachedTags);
+    }
+
+    if (this.inFlight$) {
+      return this.inFlight$;
+    }
+
+    this.inFlight$ = this.http.get<Tag[]>(this.tagsBaseUrl).pipe(
+      tap((tags) => {
+        this.cachedTags = tags;
+        this.lastFetchedAt = Date.now();
+        this.inFlight$ = null;
+      }),
+      shareReplay(1)
+    );
+
+    return this.inFlight$;
   }
 }
